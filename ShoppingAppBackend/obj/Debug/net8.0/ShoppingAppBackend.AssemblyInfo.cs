cd5a81666da5f854d2b9eb868ae918fa44fc7306
Zeki Furkan Durum <138057214+zfurkandurum@users.noreply.github.com>
2024-03-31 18:05:11 +0300

    ef core

--- ShoppingAppBackend/obj/Debug/net8.0/ShoppingAppBackend.AssemblyInfo.cs
+++ ShoppingAppBackend/obj/Debug/net8.0/ShoppingAppBackend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ShoppingAppBackend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+524e06d551eef35d7994e2d3d0c318543c0e6918")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bdbb0a2a43a0d73af0f63274f87159b8f91bac55")]
 [assembly: System.Reflection.AssemblyProductAttribute("ShoppingAppBackend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ShoppingAppBackend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
