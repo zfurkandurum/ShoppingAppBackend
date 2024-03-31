440ff874791847cc37626383839597507d0e6b2d
Zeki Furkan Durum <138057214+zfurkandurum@users.noreply.github.com>
2024-03-31 18:43:21 +0300

    Category Repository update

--- ShoppingAppBackend/Repository/CategoryRepository.cs
+++ ShoppingAppBackend/Repository/CategoryRepository.cs
@@ -1,6 +1,54 @@
+using ShoppingAppBackend.Data;
+using ShoppingAppBackend.Interfaces;
+using ShoppingAppBackend.Models;
+
 namespace ShoppingAppBackend.Repository;
 
-public class CategoryRepository
+public class CategoryRepository : ICategoryRepository
 {
+    private DataContext _context;
+
+    public CategoryRepository(DataContext context)
+    {
+        _context = context;
+    }
     
+    public ICollection<Category> GetCategories()
+    {
+        return _context.Categories.ToList();
+    }
+
+    public Category GetCategory(int categoryId)
+    {
+        return _context.Categories.Where(c => c.CategoryId == categoryId).FirstOrDefault();
+    }
+
+    public bool CategoryExist(int categoryId)
+    {
+        return _context.Categories.Any(c => c.CategoryId == categoryId);
+    }
+
+    public bool CreateCategory(Category category)
+    {
+        _context.Add(category);
+        return Save();
+    }
+
+    public bool UpdateCategory(Category category)
+    {
+        _context.Update(category);
+        return Save();
+    }
+
+    public bool DeleteCategory(Category category)
+    {
+        _context.Remove(category);
+        return Save();
+    }
+
+    public bool Save()
+    {
+        var saved = _context.SaveChanges();
+        return saved > 0 ? true : false;
+    }
 }
