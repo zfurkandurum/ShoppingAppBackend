625b431905df71714f2db40a2a7fc26746324f8f
Zeki Furkan Durum <138057214+zfurkandurum@users.noreply.github.com>
2024-04-11 23:00:52 +0300

    Models Update

--- ShoppingAppBackend/Models/Order.cs
+++ ShoppingAppBackend/Models/Order.cs
@@ -5,6 +5,6 @@ public class Order
     public int OrderId { get; set; }
     public int UserId { get; set; }
     public float TotalPrice { get; set; }
-    public User User { get; set; }
+    public ApplicationUser User { get; set; }
     public List<OrderItem> OrderItems { get; set; }
 }
