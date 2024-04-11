fe3755f6e1d6d40f114482565cde1cb9997ae35a
Zeki Furkan Durum <138057214+zfurkandurum@users.noreply.github.com>
2024-04-11 23:02:02 +0300

    DTOs Update

---
+++ ShoppingAppBackend/Dto/RegisterDto.cs
@@ -0,0 +1,11 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace ShoppingAppBackend.Dto;
+
+public class RegisterDto
+{
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string Email { get; set; }
+    public string Password { get; set; }
+}
--- ShoppingAppBackend/Dto/UserDto.cs
+++ ShoppingAppBackend/Dto/UserDto.cs
@@ -4,7 +4,10 @@ public class UserDto
 {
     public int UserId { get; set; }
     public int CartId { get; set; }
-    public int UserDetailId { get; set; }
     public string Username { get; set; }
     public string Password { get; set; }
+    public string FullName { get; set; }
+    public string Address { get; set; }
+    public string Email { get; set; }
+    public string Phone { get; set; }
 }
