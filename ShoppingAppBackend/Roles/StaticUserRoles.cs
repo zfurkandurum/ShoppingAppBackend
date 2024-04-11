beb73392e2155e7e762761a518a4be4fcee62733
Zeki Furkan Durum <138057214+zfurkandurum@users.noreply.github.com>
2024-04-11 23:03:17 +0300

    StaticUserRoles Created

---
+++ ShoppingAppBackend/Roles/StaticUserRoles.cs
@@ -0,0 +1,8 @@
+namespace JWTWithDotNetCore.Roles;
+
+public class StaticUserRoles
+{
+    public const string OWNER = "OWNER";
+    public const string ADMIN = "ADMIN";
+    public const string USER = "USER";
+}
