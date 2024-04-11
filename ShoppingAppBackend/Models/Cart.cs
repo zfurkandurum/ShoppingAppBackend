f44ee60d912f3317ce7dcd29918d8c7023763b76
Zeki Furkan Durum <138057214+zfurkandurum@users.noreply.github.com>
2024-04-11 23:01:20 +0300

    Model Update

--- ShoppingAppBackend/Models/Cart.cs
+++ ShoppingAppBackend/Models/Cart.cs
@@ -5,6 +5,7 @@ public class Cart
     public int CartId { get; set; }
     public int UserId { get; set; }
     public float Total { get; set; }
-    public User User { get; set; }
+    public ApplicationUser User { get; set; }
+    public List<Product> Products { get; set; }
     public List<CartItem> Items { get; set; }
 }
